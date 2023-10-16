bd63e794ef148a9bd22b614d500c0451007d2b45
OwnKng <owen.king@outlook.com>
2023-10-17 00:18:38 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import SocketWrapper from "@/components/SocketWrapper"
-import Cursors from "@/components/Cursors"
+import World from "@/components/World"
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <SocketWrapper>
-        <Cursors />
-      </SocketWrapper>
+      <World />
     </div>
   )
 }
--- components/World.tsx
+++ components/World.tsx
@@ -1,12 +1,52 @@
+"use client"
+
 import { Canvas } from "@react-three/fiber"
-import Player from "./Player"
-import Players from "./Players"
+import { useRef } from "react"
+
+import {
+  OrbitControls,
+  Stage,
+  Decal,
+  useTexture,
+  useGLTF,
+} from "@react-three/drei"
+import { Suspense } from "react"
 
 export default function World() {
+  const ref = useRef()
   return (
-    <Canvas>
-      <Player />
-      <Players />
-    </Canvas>
+    <div className='w-full h-screen'>
+      <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+        <Stage
+          controls={ref}
+          preset='rembrandt'
+          intensity={1}
+          environment='city'
+        >
+          <Scene />
+        </Stage>
+        <OrbitControls ref={ref} />
+      </Canvas>
+    </div>
+  )
+}
+
+function Scene() {
+  const pmndrs = useTexture("/pmndrs.png")
+  const { nodes } = useGLTF("/bust-lo-draco.glb") as any
+
+  return (
+    <Suspense fallback={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Mesh_0001.geometry}
+        position={[0, 0, 0]}
+        dispose={null}
+      >
+        <meshStandardMaterial color='black' roughness={0} metalness={0.5} />
+        <Decal position={[0, 2, 1]} scale={1} map={pmndrs} />
+      </mesh>
+    </Suspense>
   )
 }
